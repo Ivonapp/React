53200d1ffecd2aa55eedac1bb58c3aa17a0d416f
Ivonapp <ivona.v.hellsten@hotmail.com>
2025-10-28 20:52:50 +0100

    subscribe component added

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -2,19 +2,46 @@ import React, {useState, useEffect} from 'react'
 import Buttondark from './Buttondark'
 
 
-
-
 const Subscribe = () => {
+const [formData, setFormData] = useState ({ email: '', })
 
 
+const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({...formData, [name]: value })
+}
+const handleSubmit = async (e) => {
+  e.preventDefault()
+
+if (!formData.email.includes('@') || formData.email.trim() === '') {
+    alert('Vänligen ange en giltig e-postadress.');
+    return; // VILLKOR HÄR
+  }
 
 
+const res = await fetch('https://win25-jsf-assignment.azurewebsites.net/api/subscribe', {
+  method: 'post',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(formData)
+
+})
+      if (res.ok) {
+        console.log('allt lyckades')
+        alert('allt lyckades')
+        setFormData({ email: '' });
+      }
+      else {
+      console.log('Något gick fel')
+      alert('Vänligen kontrollera att du fyllt i din email korrekt')
+      }
 
 
+}
   return (
 
-<div>
-        
+        <div>
 <div className="subscribe-container">
 <div className ="icon-row">
     
@@ -26,25 +53,26 @@ const Subscribe = () => {
                  Subscribe to our newsletter to receive early discount offers, updates and info
                 </div>
                 </div>
-                <div className="search-row">
+
+
+                <form className="search-row" onSubmit={handleSubmit} noValidate>
 
                 <div className="input-wrapper">
                 <input
                 type="text"
+                name="email"
                 className="input"
                 placeholder="Enter your email"
+                value={formData.email} onChange={handleInputChange} required
                 />
-				<div id="email_error" className="error"> Please enter a valid email </div>
                 </div>
-                
                         <Buttondark
                         text="submit"
                         />
-                </div>
+                </form>
             </div>
         </div>
     </div>
-    
   )
 }
 export default Subscribe
